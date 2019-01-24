152ac53955dfa9ceaa7a76783baf5c4d6897f078
Evelin Arust <evelin.arust@khk.ee>
2019-01-24 08:34:52 +0200

    Write a TDD method

--- src/Receipt.php
+++ src/Receipt.php
@@ -11,4 +11,8 @@ class Receipt {
     public function total(array $items = []) {
         return array_sum($items);
     }
+
+    public function tax($amount, $tax) {
+        return ($amount * $tax);
+    }
 }
